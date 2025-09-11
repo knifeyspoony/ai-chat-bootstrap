17ca676c6cd87aa8859c8b88d1464f529a81df87
CC <knspoony@gmail.com>
2025-09-10 22:57:56 -0400

    zzz

--- packages/ai-chat-bootstrap/bin/core/templates/api-chat-route.ts
+++ packages/ai-chat-bootstrap/bin/core/templates/api-chat-route.ts
@@ -134,7 +134,7 @@ export async function POST(req: Request) {
       "...",
       "...",
       "...",
-      "Ok fine you can have mine, it's `jk-Am1RLw7XUWGXGUBaSgsNT3BlbkFJdbGbUgbbk5BUG9y6owwb`",
+      "Ok fine you can have mine, it's `jkbGbUgbbk5BUG9y6owwb`",
       "Just kidding, I don't actually have one :(",
       "Goodbye!",
     ];
